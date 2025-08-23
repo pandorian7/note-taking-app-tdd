bccb2e95e7d68604b925e78ecb52c4baf8ab3dc8
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-08-23 09:24:43 +0530

    New TDD BDD Project

---
+++ tdd-bdd/src/main/java/com/pandorian/tdd_bdd/TddBddApplication.java
@@ -0,0 +1,13 @@
+package com.pandorian.tdd_bdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddBddApplication.class, args);
+	}
+
+}
---
+++ tdd-bdd/src/test/java/com/pandorian/tdd_bdd/TddBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandorian.tdd_bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
