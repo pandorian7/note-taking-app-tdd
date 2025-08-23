3c368e555e5d43da09cc72967cc4fc50f2c8a8b2
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-08-23 18:16:59 +0530

    Added User Entity

--- tdd-bdd/src/main/java/com/pandorian/tdd_bdd/entity/User.java
+++ tdd-bdd/src/main/java/com/pandorian/tdd_bdd/entity/User.java
@@ -1,20 +1,35 @@
 package com.pandorian.tdd_bdd.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Data
 @Entity
+@Table(name = "users")
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class User {
 
     @Id
+    @Setter(AccessLevel.NONE)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firstName;
-
-    private String lastName;
-
+    @NonNull
     @Column(unique = true)
     private String username;
 
+    @NonNull
+    @Getter(AccessLevel.NONE)
     private String password;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    public boolean isPasswordBlank() {
+        return password.isBlank();
+    }
 }
