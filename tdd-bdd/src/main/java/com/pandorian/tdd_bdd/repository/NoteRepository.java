8f1a0e8a4b29df7684c1ffbb45f5ff5bf03f6001
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-08-24 17:30:33 +0530

    pass: add_note_success

---
+++ tdd-bdd/src/main/java/com/pandorian/tdd_bdd/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.pandorian.tdd_bdd.repository;
+
+import com.pandorian.tdd_bdd.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
--- tdd-bdd/src/main/java/com/pandorian/tdd_bdd/service/NoteService.java
+++ tdd-bdd/src/main/java/com/pandorian/tdd_bdd/service/NoteService.java
@@ -1,15 +1,20 @@
 package com.pandorian.tdd_bdd.service;
 
 import com.pandorian.tdd_bdd.entity.Note;
+import com.pandorian.tdd_bdd.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteService {
 
+    @Autowired
+    private NoteRepository noteRepository;
+
     public final static int MAXIMUM_NOTE_CONTENT_LENGTH = 100;
 
     public Note addNote(Note note) {
-        return new Note();
+        return noteRepository.save(note);
     }
 
     public Note modifyNote(Note note) {
