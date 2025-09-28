ff768896b818cfe71e58b598703249a01dc8d61c
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-09-29 01:03:17 +0530

    basic pages

---
+++ user-interface/src/pages/Notes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notes = () => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <p>Your notes will appear here</p>
+    </div>
+  )
+}
+
+export default Notes
---
+++ user-interface/src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>Sign in to your account</p>
+    </div>
+  )
+}
+
+export default SignIn
